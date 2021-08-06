21d72340eaff64532b419224ad33460a3c0981d3
Vaishak Salin <vaishak93@gmail.com>
2021-08-06 13:52:07 -0700

    Visuals
    Former-commit-id: b4c690af3a8efcae7d5ca29226c5e434f7666ccf

---
+++ visuals/crashed-spaceship-map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="crashed-spaceship-map" tilewidth="4100" tileheight="2475" tilecount="1" columns="1">
+ <image source="crashed-spaceship-map.png" width="4100" height="2475"/>
+</tileset>
