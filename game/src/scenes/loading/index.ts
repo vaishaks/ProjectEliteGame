2bc8c7434589df48c3c23eee5d27ddc8e54104fd
Vaishak Salin <vaishak93@gmail.com>
2021-08-06 13:51:41 -0700

    Added more assets
    Former-commit-id: 81cace81232e02a469f6f3d005e3bff1c9c2b490

--- game/src/scenes/loading/index.ts
+++ game/src/scenes/loading/index.ts
@@ -8,15 +8,22 @@ export class LoadingScene extends Scene {
     private gridLayer!: Tilemaps.TilemapLayer;
     private player!: GameObjects.Image;
     private backgroundLayer!: Tilemaps.TilemapLayer;
+    private scaleNumber: number;
+
     constructor() {
         super('loading-scene');
+        this.scaleNumber = 1;
     }
 
     preload(): void {
         this.load.baseURL = 'assets/';
-        this.load.image({ key: 'project-elite-map-tiles', url: 'project-elite-03.png' });
+        this.load.image({ key: 'crashed-spaceship-map', url: 'crashed-spaceship-map-v3.png' });
         this.load.image({ key: 'player-gherid', url: 'gherid-v4.png' });
-        this.load.tilemapTiledJSON('project-elite-map', 'crashed-map-1.json');
+        this.load.image({ key: 'player-akosha', url: 'akosha-v1.png' });
+        this.load.image({ key: 'enemy-runner', url: 'runner-v1.png' });
+        this.load.image({ key: 'enemy-biter', url: 'biter-v1.png' });
+        this.load.image({ key: 'enemy-shooter', url: 'shooter-v1.png' });
+        this.load.tilemapTiledJSON('crashed-spaceship-map', 'crashed-spaceship-map.json');
     }
 
     create(): void {
@@ -25,20 +32,28 @@ export class LoadingScene extends Scene {
     }
 
     private initMap(): void {
-        this.map = this.make.tilemap({ key: 'project-elite-map', tileWidth: 2040, tileHeight: 1212 });
-        this.tileset = this.map.addTilesetImage('project-elite-map', 'project-elite-map-tiles');
+        this.map = this.make.tilemap({ key: 'crashed-spaceship-map', tileWidth: 4100, tileHeight: 2475 });
+        this.tileset = this.map.addTilesetImage('crashed-spaceship-map-tileset', 'crashed-spaceship-map');
         this.backgroundLayer = this.map.createLayer('Background', this.tileset, 0, 0);
-        this.backgroundLayer.setScale(0.6);
-        
+        this.scaleNumber = 0.3;
+        console.log(this.scaleNumber);
+        this.backgroundLayer.setScale(this.scaleNumber);
+
 
         const gridPoints = gameObjectsToObjectPoints(
-            this.map.filterObjects('Grid', obj => obj.name === 'point'),
+            this.map.filterObjects('Grid', obj => obj.name === 'squares'),
         );
 
-        const ix = 1;
-        this.player = this.add.sprite(gridPoints[ix].x*0.6, gridPoints[ix].y*0.6, 'player-gherid');
-        this.player.setScale(0.35);
-
-        console.log(gridPoints);
+        let sprites = ['', 'player-gherid', 'enemy-runner', 'enemy-runner', 'enemy-biter', 'enemy-shooter', 'enemy-shooter', 'enemy-shooter', 'player-akosha']
+        for (let ix = 1; ix < 9; ix++)
+        {
+            this.add
+            .sprite(
+                (gridPoints[ix].x + gridPoints[ix].width / 2) * this.scaleNumber,
+                (gridPoints[ix].y + gridPoints[ix].height / 2) * this.scaleNumber,
+                sprites[ix]
+            )
+            .setScale(this.scaleNumber)            
+        }
     }
 }
