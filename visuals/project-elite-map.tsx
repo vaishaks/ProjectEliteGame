41ae6684d1b1fa95b739bf2224e0009b27b4f85f
Vaishak Salin <vaishak93@gmail.com>
2021-08-09 14:48:58 -0700

    Delete big files
    Former-commit-id: ed31c68d7c7e7c9a95e4f83b50e8a273f70d2bcf

--- visuals/project-elite-map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="project-elite-map" tilewidth="2040" tileheight="1212" tilecount="1" columns="1">
- <image source="project-elite-03.png" width="2040" height="1212"/>
-</tileset>
